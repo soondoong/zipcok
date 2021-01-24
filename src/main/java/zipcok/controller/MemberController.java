69e6768ea3a25ff5fc2c18659818e170f396f953
well <well@Cdr-02008041822>
2021-01-24 17:01:25 +0900

    회원가입 진행중입니다~

--- src/main/java/zipcok/controller/MemberController.java
+++ src/main/java/zipcok/controller/MemberController.java
@@ -1,8 +1,13 @@
 package zipcok.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MemberController {
 
+	@RequestMapping("/memberJoin.do")
+	public String memberJoinForm() {
+		return "member/memberJoin";
+	}
 }
