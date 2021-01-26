91fd57d1f361b9943203618dceacebc498d0e3dc
azrael1994 <azrael@DESKTOP-6H7N20R>
2021-01-26 19:48:54 +0900

    운동 기구 등록

--- src/main/java/zipcok/homegym/model/HomeGymEquipmentDAO.java
+++ src/main/java/zipcok/homegym/model/HomeGymEquipmentDAO.java
@@ -1,9 +1,10 @@
 package zipcok.homegym.model;
 
 import java.util.List;
+import java.util.Map;
 
 public interface HomeGymEquipmentDAO {
-	public int HomeGymEquipmentAdd(HomeGymEquipmentDTO dto);
+	public int HomeGymEquipmentAdd(Map<String, Object> map);
 	public List<HomeGymEquipmentDTO> HomeGymEquipmentSelect(String id);
 	
 }
