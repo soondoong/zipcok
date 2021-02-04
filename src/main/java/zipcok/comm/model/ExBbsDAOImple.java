a162cfc09acb8ae6dcc776326e9f5df25fafc62d
user <user@NOTE>
2021-02-04 19:30:06 +0900

    게시판 파일 삭제 글 수정 시 파일 삭제 구현 중

--- src/main/java/zipcok/comm/model/ExBbsDAOImple.java
+++ src/main/java/zipcok/comm/model/ExBbsDAOImple.java
@@ -11,12 +11,14 @@ public class ExBbsDAOImple implements ExBbsDAO {
 	@Autowired
 	private SqlSessionTemplate sqlMap;
 	
+	//글쓰기
 	@Override
 	public int dailyWrite(ExBbsDTO dto) {
 		int count=sqlMap.insert("dailyWriteSQL", dto);
 		return count;
 	}
 	
+	//글목록 불러오기
 	@Override
 	public List dailyList(int cp, int ls) {
 		int start=(cp-1)*ls+1;
@@ -28,72 +30,84 @@ public class ExBbsDAOImple implements ExBbsDAO {
 		return list;
 	}
 	
+	//총 게시물 수 구하기
 	@Override
 	public int getTotalCnt() {
 		int count=sqlMap.selectOne("getTotalCntSQL");
 		return count==0?1:count;
 	}
 	
+	//글 보기
 	@Override
 	public ExBbsDTO dailyContent(int ex_idx) {
 		ExBbsDTO dto=sqlMap.selectOne("bbsContentSQL",ex_idx);
 		return dto;
 	}
 	
+	//글 수정
 	@Override
 	public int dailyUpdate(ExBbsDTO dto) {
 		int count=sqlMap.update("bbsUpdateSQL",dto);
 		return count;
 	}
 	
+	//글 삭제
 	@Override
 	public int dailyDelete(int ex_idx) {
 		int count=sqlMap.delete("bbsDeleteSQL",ex_idx);
 		return count;
 	}
 	
+	//조회수 불러오기
 	@Override
 	public int dailyReadnum(int ex_idx) {
 		int count=sqlMap.update("bbsReadnumSQL",ex_idx);
 		return count;
 	}
 	
+	//댓글 최대 순번 구하기
 	@Override
 	public int dailyGetMaxSunbun(int re_idx) {
 		int count=sqlMap.selectOne("dailyGetMaxSunbunSQL",re_idx);
 		return count;
 	}
 	
+	//댓글 쓰기
 	@Override
 	public int dailyReWrite(ExReBbsDTO dto) {
 		int count=sqlMap.insert("bbsReWriteSQL",dto);
 		return count;
 	}
 	
+	//댓글 삭제
 	@Override
 	public int dailyReDelete(int re_group) {
 		int count=sqlMap.delete("bbsReDeleteSQL",re_group);
 		return count;
 	}
 	
+	//대댓글 삭제
 	@Override
 	public int dailyReReDelete(int re_idx) {
 		int count=sqlMap.delete("bbsReReDeleteSQL",re_idx);
 		return count;
 	}
 	
+	//댓글 뿌려주기
 	@Override
 	public List dailyReList(int re_bbs_idx) {
 		List list=sqlMap.selectList("dailyReListSQL",re_bbs_idx);
 		return list;
 	}
 	
+	//댓글 갯수 구하기
 	@Override
 	public int dailyGetTotalRe(int re_bbs_idx) {
 		int count=sqlMap.selectOne("dailyGetTotalReSQL",re_bbs_idx);
 		return count;
 	}
 	
+	//총 댓글 갯수 입력하기
 	@Override
 	public int dailySetTotalRe(int ex_recnt, int ex_idx) {
 		Map map=new HashMap();
@@ -103,18 +117,21 @@ public class ExBbsDAOImple implements ExBbsDAO {
 		return count;
 	}
 	
+	//부모댓글 순번구하기
 	@Override
 	public int dailyGetSunbun_p(int re_group) {
 		int result=sqlMap.selectOne("dailyGetSunbun_pSQL", re_group);
 		return result;
 	}
 	
+	//부모댓글 레벨구하기
 	@Override
 	public int dailyGetLev(int re_idx) {
 		int result=sqlMap.selectOne("dailyGetLevSQL", re_idx);
 		return result;
 	}
 	
+	//댓글 순서 업데이트
 	@Override
 	public void dailyReUpdate(int re_bbs_idx, int re_sunbun) {
 		Map map=new HashMap();
@@ -123,6 +140,7 @@ public class ExBbsDAOImple implements ExBbsDAO {
 		int result=sqlMap.update("dailyReUpdateSQL", map);
 	}
 	
+	//대댓글 쓰기
 	@Override
 	public int dailyReReWrite(ExReBbsDTO dto) {
 		int count=sqlMap.insert("dailyReReWriteSQL",dto);
